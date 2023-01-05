b8a447e739bed36be41c7b7479878046e87bbec9
IlyassElmoutaoukkil <31111186+IlyassElmoutaoukkil@users.noreply.github.com>
2023-01-05 22:31:08 +0100

    robot site is well handled

--- main.ts
+++ main.ts
@@ -1,21 +1,66 @@
-let index:number = 0
-let movments = [[60, 58, 2000]]
+serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
+    data = serial.readString()
+    let dataLetter = data.substr(0,1)
+    if (dataLetter == "S") {
+        basic.showLeds(`
+            . . # . .
+            . . # # .
+            . . # # #
+            . . # # .
+            . . # . .
+            `)
+        startRecievingStatus = "StartSaving"
+        console.log('Start saving...')
+music.playMelody("C C - - - - - - ", 999)
+    } else if (dataLetter == "E") {
+        basic.showLeds(`
+            # # . # #
+            # # . # #
+            # # . # #
+            # # . # #
+            # # . # #
+            `)
+        startRecievingStatus = "END"
+        console.log('End saving...')
+music.playMelody("C5 C5 - - - - - - ", 999)
+    } else {
+        music.playMelody("- - - - F F F F ", 999)
+        console.log(data + ' saved.')
+dataSplit = data.split("|")
+        movments.push([parseFloat(dataSplit[0]), parseFloat(dataSplit[1]), parseFloat(dataSplit[2])])
+    }
+})
+let dataSplit: string[] = []
+let startRecievingStatus2 = ""
+let data2: number[] = []
+let movments: any[][] = []
+let index: number = 0
+let data = ""
+let startRecievingStatus = ""
+serial.redirectToUSB()
+let startRecievingStatus3 = "initial"
 function controllPen(move: any) {
     if (move == 'open') {
         return SuperBit.Servo(SuperBit.enServo.S1, 38)
     }
     return SuperBit.Servo(SuperBit.enServo.S1, 50)
 }
-radio.onReceivedString(function(receivedString: string) {
-    let data:any[] = receivedString.split('|')
-    movments.push([parseFloat(data[0]), parseFloat(data[1]),parseFloat(data[2])])
-})
-
 controllPen('close')
 controllPen('open')
-radio.setGroup(1)
 basic.forever(function () {
-    if (movments.length > 0) {
+if (movments.length > 0 && startRecievingStatus == "END") {
+        if(index==0){
+            pause(5000)
+            basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+        }
+    
+
         if (index < movments.length) {
             SuperBit.MotorRunDual(
             SuperBit.enMotors.M2,
@@ -30,8 +75,7 @@ SuperBit.MotorRunDual(
             SuperBit.enMotors.M4,
             0
             )
-            console.log(movments[index])
-index += 1
+            index += 1
         } else {
             index += 0
             movments = []
